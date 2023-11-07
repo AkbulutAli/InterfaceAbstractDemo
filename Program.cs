703882dc2c69d67df4253152524b76c210403985
akbul <akbul@DESKTOP-9J8GTAC>
2023-11-07 13:02:02 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+
+namespace InterfaceAbstractDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter() );
+            customerManager.Save(new Entities.Customer { DateOfBirth = new DateTime(1999, 3, 12), FirstName = "Ali Sefa", LastName = "AKBULUT", NationalityId = "123" });
+        }
+    }
+
+    
+}
